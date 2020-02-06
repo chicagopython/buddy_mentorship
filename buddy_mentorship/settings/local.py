5fa00e07317b9a5b7e8f4f5fa05147aeca4bc967
Zax Rosenberg <zax.rosenberg@gmail.com>
2020-02-05 20:00:42 -0600

    Add django bootstrap and base.html with nav menu and basic layout

--- buddy_mentorship/settings/local.py
+++ buddy_mentorship/settings/local.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third-party apps
+    'bootstrap4',
     # internal
     "apps.users",
     "buddy_mentorship",
@@ -95,9 +96,9 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", },
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator", },
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator", },
 ]
 
 
