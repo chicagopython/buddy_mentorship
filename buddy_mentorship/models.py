9f7167304dc703a6970d2c4fc7bbcd77d7999630
Emily Xia-Reinert <emilycxia@gmail.com>
2020-02-23 14:34:03 -0600

    added BuddyRequest model

---
+++ buddy_mentorship/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.utils import timezone
+from apps.users.models import User
+
+
+class BuddyRequest(models.Model):
+    request_sent = models.DateTimeField(default=timezone.now)
+    requestee = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="requestee"
+    )
+    requestor = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="requestor"
+    )
+    message = models.CharField(max_length=511)
+
+    def __str__(self):
+        return (
+            "Buddy request from "
+            f"{self.requestor.email} to {self.requestee.email} on "
+            f"{self.request_sent}"
+        )
