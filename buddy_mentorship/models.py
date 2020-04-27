df00989468d3829ac6f2c5e14fcdfa00860410e9
Emily Xia-Reinert <emilycxia@gmail.com>
2020-04-26 19:08:27 -0500

    adds short bio for search view

--- buddy_mentorship/models.py
+++ buddy_mentorship/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.utils import timezone
 from apps.users.models import User
 
-
 class BuddyRequest(models.Model):
     request_sent = models.DateTimeField(default=timezone.now)
     requestee = models.ForeignKey(
@@ -32,4 +31,17 @@ class Profile(models.Model):
     can_help = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.bio
+        return f"Profile for {self.user.email}"
+
+    def get_short_bio(self):
+        trunc_bio = self.bio[:240]
+        first_nl = trunc_bio.find("\n")
+        if first_nl > -1:
+            return trunc_bio[:first_nl]
+        last_dot = trunc_bio.rfind(".")
+        last_bang = trunc_bio.rfind("!")
+        last_huh = trunc_bio.rfind("?")
+        last_sentence = max(last_dot, last_bang, last_huh)
+        if last_sentence > -1:
+            return trunc_bio[:last_sentence+1]
+        return trunc_bio[:trunc_bio.rfind(" ")+1]
